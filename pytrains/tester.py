6dba515f4950a835d7ce4fa04416bc69f7276161
Chris Withers <chris@simplistix.co.uk>
2012-08-28 08:32:26 +0100

    Better printing of yielded messages.

--- pytrains/tester.py
+++ pytrains/tester.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import datetime
 from pytrains.cif.parser import parse
 
 import sys
@@ -6,14 +7,29 @@ import sys
 def main():
     i = 0
     types = defaultdict(int)
-    for message in parse(open(sys.argv[1])):
-        if isinstance(message, Exception):
-            raise message
-        else:
-            print '===%s===' % message.__class__.__name__
-            for name, value in zip(message._fields, message):
-                print '%20s=%r' % (name, value)
-            print
-            
+    to_print = set(sys.argv[2:])
+    try:
+        started = datetime.now()
+        for message in parse(open(sys.argv[1])):
+            types[message.prefix] += 1
+            from .cif.meta import ongoing
+            if isinstance(message, Exception):
+                pass
+                #raise message
+            elif message.prefix in to_print:
+                print '===%s===' % message.__class__.__name__
+                for name, value in zip(message._fields, message):
+                    print '%20s=%r' % (name, value)
+                print
+            if not i % 10000:
+                print types
+            i += 1
+    finally:
+        duration = datetime.now() - started
+        print "%i records in %s at %.0f/s" % (
+            i, duration,
+            float(i)/duration.seconds if duration.seconds else i
+            )
+        
 if __name__=='__main__':
     main()
